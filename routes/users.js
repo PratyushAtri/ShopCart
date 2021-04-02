0275d9dcc8cf541dfdb632c71bd9a58f92bb8c3a
PratyushAtri <atripratyush16@gmail.com>
2021-04-02 11:13:34 +0530

    Changes in User route

--- routes/users.js
+++ routes/users.js
@@ -7,11 +7,11 @@ const router = express.Router();
 
 const User = require('../models/User');
 
+// Register User // JWT token returned for success
 router.post ('/', [
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Please include a valid email').isEmail(),
-    check('password', 'Please Enter a password with 5 or more characters').isLength({ min: 5 }),
-    check('Admin', 'Wanna be an admin? Enter admin  password')
+    check('password', 'Please Enter a password with 5 or more characters').isLength({ min: 5 })
 ], async (req, res) => {
    
     const errors = validationResult(req);
@@ -21,6 +21,7 @@ router.post ('/', [
     }
 
     const { name, email, password, adminPass } = req.body;
+    const adminCorrectPass = 94038020440540460420160;
     let admin = false;
 
     try {
@@ -28,14 +29,18 @@ router.post ('/', [
         // See if the user exists
         let user = await User.findOne({ name });  
         
+        // user already exists
         if (user) {
             return res.status(400).json({ errors: [{ msg: `User ${name} already exists` }] });
         }
 
-        if (adminPass === 94038020440540460420160) {
+        // user does not exist
+        // see if the user is admin
+        if (adminPass === adminCorrectPass) {
             admin = true;
         }
 
+        // save the user ini database 
         user = new User({
             name,
             email,
